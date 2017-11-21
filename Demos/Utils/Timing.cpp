46328868d13ee815721992d0de442b7876c267b7
Jan Bender <bender@cs.rwth-aachen.de>
2017-11-21 08:00:03 +0100

    - removed old files

--- Demos/Utils/Timing.cpp
+++
@@ -1,9 +0,0 @@
-#include "Timing.h"
-
-using namespace PBD;
-
-std::unordered_map<int, AverageTime> Timing::m_averageTimes;
-std::stack<TimingHelper> Timing::m_timingStack;
-bool Timing::m_dontPrintTimes = false;
-unsigned int Timing::m_startCounter = 0;
-unsigned int Timing::m_stopCounter = 0;
