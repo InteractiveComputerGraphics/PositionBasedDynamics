a4305923cb82c1005fbc072cca58b173d757025f
ruberith <robin.rademacher@rwth-aachen.de>
2022-09-15 14:21:17 +0200

    Update Discregrid reference and fix version inclusion

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ with open(os.path.join(cur_dir, "README.md"), 'r') as f:
     long_description = f.read()
 	
 # read version	
-f = open("version.txt", "r")
+f = open("version", "r")
 pbd_version = f.readline().strip()
 f.close() 
 
